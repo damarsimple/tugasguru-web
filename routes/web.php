de878f747d73f886b848d1620491f16c3c10726a
Damar Albaribin <damaralbaribin@gmail.com>
2021-05-15 17:02:13 +0700

    fix : attachment route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/attachments/{path}', function ($path) {
+    return response()
+        ->file(public_path('attachments')  . '/' . $path, ['Access-Control-Allow-Origin' => '*']);
+});
