bbb4efdebe832a08bb9e5cbdeb93d2d83450ff61
Damar Albaribin <damaralbaribin@gmail.com>
2021-06-13 23:28:30 +0700

    fix : forms

--- app/Models/Form.php
+++ app/Models/Form.php
@@ -8,16 +8,18 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Form extends Model
 {
-    const REQUEST_TUTOR = "REQUEST_TUTOR";
-    const REQUEST_COUNSELOR = "REQUEST_COUNSELOR";
-    const REQUEST_HEADMASTER = "REQUEST_HEADMASTER";
-    const REQUEST_PPDB = "REQUEST_PPDB";
-    const REQUEST_HOMEROOM = "REQUEST_HOMEROOM";
+    public const REQUEST_TUTOR = "REQUEST_TUTOR";
+    public const REQUEST_COUNSELOR = "REQUEST_COUNSELOR";
+    public const REQUEST_HEADMASTER = "REQUEST_HEADMASTER";
+    public const REQUEST_PPDB = "REQUEST_PPDB";
+    public const REQUEST_HOMEROOM = "REQUEST_HOMEROOM";
 
-    const PENDING = 0;
-    const PROCESSED = 1;
-    const FINISHED = 2;
-    const REJECTED = 3;
+    public const PENDING = 0;
+    public const PROCESSED = 1;
+    public const FINISHED = 2;
+    public const REJECTED = 3;
+
+    public const DOCUMENTS  = 'DOCUMENTS';
 
     use HasFactory;
 
