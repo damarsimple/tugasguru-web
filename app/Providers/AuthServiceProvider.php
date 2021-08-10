9c4fbeed3edf2c7739eb398e73ac3b0cb01a7927
Damar Albaribin <damaralbaribin@gmail.com>
2021-08-10 10:33:53 +0700

    fix : change languange

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Konfirmasi Alamat Email')
+                ->line('Tekan tombol dibawah untuk memverifikasi email anda.')
+                ->action('Verifikasi Alamat Email', $url);
+        });
     }
 }
